a4e90249bf7b6648204f619e79a60d9eb2ddf125
Vini0410 <vini0420@gmail.com>
2020-12-02 19:49:57 -0300

    envio n3

--- N3_vinicius/main.py
+++ N3_vinicius/main.py
@@ -74,19 +74,5 @@ def update_musica(id):
     else:
         return not_found()
 
-
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': "Not Found" + request.url
-    }
-    resp = jsonify(message)
-
-    resp.status_code = 404
-
-    return resp
-
-
 if __name__ == "__main__":
     app.run(debug=True)
